8059cf2f7ef8b521cb0e1da78f7f485ab0072088
Samplasion <samplasion@gmail.com>
2018-09-21 18:37:11 +0200

    Add #pushArr(arr) and static .fromArr(arr) methods.
    Version 1.1.0

--- src/List.js
+++ src/List.js
@@ -3,6 +3,16 @@ class List extends Array {
     super(...args)
     console.log(typeof this)
   }
+  
+  static fromArr(arr) {
+	var list = new List(arr.length)
+	arr.forEach(i => list.push(i))
+	return list
+  }
+  
+  pushArr(arr) {
+	arr.forEach(i => this.push(i))
+  }
 
   random() {
     return this[Math.floor(Math.random()*this.length)]
